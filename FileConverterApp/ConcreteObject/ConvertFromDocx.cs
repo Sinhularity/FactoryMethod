9a14d21139e102120d594442063095ae63cf7cb8
Sinhularity <114500120+Sinhularity@users.noreply.github.com>
2024-09-19 13:34:32 -0600

    Update ConvertFromDocx.cs

--- FileConverterApp/ConcreteObject/ConvertFromDocx.cs
+++ FileConverterApp/ConcreteObject/ConvertFromDocx.cs
@@ -1,42 +1,23 @@
-using Spire.Doc;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.PixelFormats;
+using Aspose.Words;
 
-class ConvertFromDocx : IFileConverter
+class ConvertFromPDF : IFileConverter
 {
     private string defaultPath = IFileConverter.DEFAULT_PATH;
-    private string[] files = IFileConverter.ACTUAL_FILES;
-    public const string EXTENSION = ".docx";
-    public const string OUTPUT_EXTENSION = IFileConverter.OUTPUT_EXTENSION;
+    public const string EXTENSION = ".pdf";
+    private const string OUTPUT_EXTENSION = IFileConverter.OUTPUT_EXTENSION;
+
     public void Conversion(string fileName)
     {
         string inputFile = fileName + EXTENSION;
         string outputFile;
         string inputPath = Path.Combine(defaultPath, inputFile);
         string outputPath;
-        Document docxDocument = new Document();
-
-        docxDocument.LoadFromFile(inputPath);
-
-        for (int i = 0; i < docxDocument.PageCount; i++)
+        
+        var doc = new Document(inputPath);
+        for (int i = 0; i < doc.PageCount; i++)
         {
-            using (System.Drawing.Image pageImage = docxDocument.SaveToImages(i, Spire.Doc.Documents.ImageType.Bitmap))
-            {
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    pageImage.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
-                    ms.Seek(0, SeekOrigin.Begin);
-
-                    using (var image = SixLabors.ImageSharp.Image.Load<Rgba32>(ms))
-                    {
-                        // Guardar la imagen como JPG
-                        outputFile = fileName + "_page" + (i + 1) + OUTPUT_EXTENSION;
-                        outputPath = Path.Combine(defaultPath, outputFile);
-                        image.Save(outputPath);
-                    }
-                }
-            }
-            Console.WriteLine($"JPG {outputFile} creado en {outputPath}");
+            var extractedPage = doc.ExtractPages(i, 1);
+            extractedPage.Save(fileName + i + OUTPUT_EXTENSION);
         }
     }
 }
